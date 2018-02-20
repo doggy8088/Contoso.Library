969de25fe778b8c6cdfb0703c00b09265e4d7715
Will <doggy.huang@gmail.com>
2018-02-20 20:46:30 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Contoso.Library;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = new MyTool();
+            o.Run();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Contoso.Library/MyTool.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contoso.Library
+{
+    public class MyTool
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World! 2");
+        }
+    }
+}
